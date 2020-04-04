32f09bf6b5e75304efad8b87e1158c6677bf857b
erkha <jmtatout@gmail.com>
2020-04-04 19:33:37 +0200

    angular quest 02- binding

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+  User = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : '',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+  
+  hideAge: boolean;
+
+  displayAge():void {
+    this.hideAge = !this.hideAge ;
+  }
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  this.hideAge = true;
+  }
+
+}
